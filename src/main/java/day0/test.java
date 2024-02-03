7e34b4c81602c182bd4430ac195ae7c72dd438d6
chaicka007 <sashasa534@gmail.com>
2024-02-03 16:10:48 +0400

    Scanner test

--- src/main/java/day0/test.java
+++ src/main/java/day0/test.java
@@ -1,11 +1,23 @@
 package day0;
-
+import java.util.Scanner;
 public class test {
     public static void main(String [] args){
-        int x = 10;
-        String superString = "тестовая строка";
-        String string2 = " Тестовая строка 2 ";
-        System.out.println(superString + string2);
-        System.out.println("test number " + x);
+        int value = -3;
+        while(value!=1){
+            System.out.println(value);
+            ++value;
+        }
+        for(byte i = 0; i <=5; i+=2){
+            System.out.println("test2, i= " + i);
+        }
+        int x = 5;
+        float y  = 6;
+
+        System.out.printf("x=%d; y=%.3243f", x, y);
+
+        Scanner in  = new Scanner(System.in);
+        System.out.println("\ngive me a number: ");
+        int number = in.nextInt();
+        System.out.println("number is "+ number);
     }
 }
