8af77b2905a1edb8a37b26e18536b1b2e8381f78
chaicka007 <sashasa534@gmail.com>
2024-02-20 21:10:36 +0400

    day 7

--- src/main/java/day7/Player.java
+++ src/main/java/day7/Player.java
@@ -1,7 +1,71 @@
 package day7;
 
+import java.text.MessageFormat;
+import java.util.Random;
+
 public class Player {
     private int stamina;
+    private boolean onField; //на футбольном ли поле?
+    private static int countPlayers;
+    private static int inReserveCount;
     public static final int MAX_STAMINA = 100;
     public static final int MIN_STAMINA = 0;
-}
+
+    public Player(int stamina) { //создание игрока
+        this.stamina = stamina;
+        // Random rand = new Random();
+        //this.stamina = rand.nextInt(MAX_STAMINA - 90 + 1) + 90;
+        if (countPlayers <6){
+            this.onField = true;
+            countPlayers++;
+        } else{
+            inReserveCount++;
+            this.onField = false;
+        }
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public static int getCountPlayers() {
+        return countPlayers;
+    }
+
+    public void run() {
+
+        if (this.onField) {
+            this.stamina -= 1;
+            if (this.stamina == MIN_STAMINA) {
+                this.onField = false;
+                System.out.println("Чел выдохся и уходит с поля");
+                inReserveCount++;
+                countPlayers--;
+            }
+        } else {
+            System.out.println("Чел не на поле");
+        }
+
+    }
+    public static void info(){
+        if (countPlayers <6){
+            System.out.printf("Команды неполные. На поле еще есть %d" + declension(), 6- countPlayers);
+        } else {
+            System.out.print("На поле нет свободных мест\n");
+        }
+    }
+    private static String declension(){
+        switch (6-countPlayers) {
+            case 1:
+                return " свободное место\n";
+            case 2:
+            case 3:
+            case 4:
+                return " свободных места\n";
+            default:
+                return " свободных мест\n";
+
+        }
+        }
+    }
+
--- src/main/java/day7/Task2.java
+++ src/main/java/day7/Task2.java
@@ -2,6 +2,21 @@ package day7;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Player p1 = new Player(90);
+        Player.info();
+        Player p2 = new Player(60);
+        Player p3 = new Player(50);
+        Player.info();
+        Player p4 = new Player(5);
+        Player p5 = new Player(5);
+        Player.info();
+        Player p6 = new Player(1);
+        Player.info();
+        Player p7 = new Player(5);
+        Player p8 = new Player(1);
+        System.out.println(Player.getCountPlayers());
+        p8.run();
+        p6.run();
+        Player.info();
     }
 }
